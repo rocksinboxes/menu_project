98941aa8d5dafc078e4588b9de5630526eb02ac6
rocksinboxes <rocksinboxes@gmail.com>
2021-03-21 10:37:28 -0700

    Fixed stuff

--- packages.py
+++ packages.py
@@ -2,16 +2,29 @@
 from subprocess import run
 from file_download import download_file
 from system_checks import system_check
+import github_repo
 
 
 class nodejs:
-    def __init__(self) -> None:
-        self.name = "nodejs"
-        self.download = download_file("https://deb.nodesource.com/setup_15.x")
+        
 
     def nodejs_install(self):
+        self.name=name
+        self.name = "nodejs"
+        self.download = download_file("https://deb.nodesource.com/setup_15.x")
         self.download.download_installer()
-        run(["sudo", "apt", "install", self.name, "-y"])
+        run(["sudo", "apt", "install", name, "-y"])
 
-    def nodejs_update(self):
+    def nodejs_update():
         system_check.apt_system_checks()
+
+class asdf:
+    def asdf_install():
+        name="asdf-vm"
+        repo="asdf"
+        local_repo=".asdf"
+        github_repo.define_remote_repo.clone_remote_repo(name,repo,local_repo)
+
+        
+
+test = asdf.asdf_install()
