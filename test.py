85e3c9b0490b013e01613138c8df672d3a3e4e9a
rocksinboxes <rocksinboxes@gmail.com>
2021-03-25 12:37:39 -0700

    Changes

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-     ''' tmppath=tempfile.NamedTemporaryFile(dir='/home/pi/', delete=False,suffix='.sh')
-        original_path=tmppath.name
-        with open(original_path, 'wb') as f:
-            f.write(data)
-            print(original_path)
-            chmod(original_path, 0o755)
-            call(original_path)
-            f.close()
-         '''   
-            
+print('Hello', 'how are you? test')
