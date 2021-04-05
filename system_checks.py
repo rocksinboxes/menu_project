a08c364b1fcb0f0158999ee3012243f3e90ad5e0
Robert Lemon <rocksinboxes@gmail.com>
2021-04-05 11:11:06 -0700

    fixed source

--- system_checks.py
+++ system_checks.py
@@ -4,20 +4,20 @@ from platform import python_version, architecture
 
 
 class system_check:
-    def __init__(self) -> None:
-        self.minimum_python_version = "3.7.3"
-        self.minimum_distro_version = "buster"
+    def __init__() -> None:
+        minimum_python_version = "3.7.3"
+        minimum_distro_version = "buster"
 
     def apt_system_checks():
         call(["sudo", "apt", "update"])
         call(["sudo", "apt", "upgrade", "-y"])
 
-    def rasp_os_ver(self):
+    def rasp_os_ver():
         os_file = "/etc/os-release"
         with open(os_file, 'r') as f:
             data = f.read()
             data = data.splitlines()[4].split("=")[1]
-            if data != self.minimum_distro_version:
+            if data != system_check.minimum_distro_version:
                 f.close()
                 exit("Unsupported Distro")
             f.close()
@@ -29,14 +29,15 @@ class system_check:
             exit("Invalid Arch")
         return plat[0]
 
-    def get_python_version(self):
-        self.python_version = python_version()
-        if self.python_version < self.minimum_python_version:
+    def get_python_version():
+        python_version = python_version()
+        if system_check.python_version < system_check.minimum_python_version:
             exit("Your Python Version is too low")
-        return self.python_version
+        return system_check.python_version
 
     def install_required_pip_packages():
-        call(["pip", "install", "--upgrade" "pip", "systemtools", "disttools", "wheel"])
+        call(["pip", "install", "--upgrade" "pip",
+             "systemtools", "disttools", "wheel"])
         with open('/home/pi/menu_project/requirements.txt', 'r') as f:
             for line in f:
                 data = line.strip("\n").split()[0]
